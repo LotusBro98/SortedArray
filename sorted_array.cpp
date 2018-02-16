738c8adf1de181cf000a2f240dfadfa96e1f62c4
BuildTools <unconfigured@null.spigotmc.org>
2018-02-16 21:18:24 +0300

    Added sacreate function

---
+++ sorted_array.cpp
@@ -0,0 +1,32 @@
+struct sorted_array
+{
+	void* buffer;
+	size_t elem_size;
+	size_t max_elems;
+
+	int (*compar)(void* a, void* b);
+
+	size_t n;
+}
+
+struct sorted_array* sacreate(size_t elem_size, size_t max_elems, int (*compar)(void* a, void* b))
+{
+	struct sorted_array* array = (struct sorted_array*) malloc(sizeof(struct sorted_array));
+	if (array == NULL)
+		return NULL;
+
+	array->buffer = malloc(elem_size * max_elems);
+	if (array->buffer == NULL)
+	{
+		free(array);
+		return NULL;
+	}
+
+	array->max_elems = max_elems;
+	array->elem_size = elem_size;
+	array->compar = compar;
+
+	array->n = 0;
+
+	return array;
+}
--- sorted_array.h
+++ sorted_array.h
@@ -4,8 +4,8 @@
  * Here you can find declarations and descriptions of:
  * - struct sotred_array;
  * - functions for creating and destroying a sorted array:
- *   + saalloc();
- *   + safree();
+ *   + sacreate();
+ *   + sadelete();
  * - functions for working with elements:
  *   + saput();
  *   + saget();
@@ -26,7 +26,7 @@
 
 
 /** @struct sorted_array
- * Structure representing a static sorted array.
+ * Structure representing a static array that is always sorted.
  *
  * This array can contain elements of any size, but all elements in one array are of the same size.
  * The elements are stored one by one in a buffer allocated in heap.
@@ -40,5 +40,19 @@
  * @note If you encounter a situation when you need to apply some changes to the elements that can affect their order 
  *  (e.g. You are storing pointers to some elements, and you also have pointers to them in other places of the program), 
  *  call saresort() function to sort the array again.
+ * @note The array is sorted in ascending order.
  */
  struct sorted_array;
+
+/**
+ * Create a new sorted array.
+ *
+ * @param max_elems max array length 
+ * @param elem_size size of one element in bytes
+ * @param compar specifies a comparator function. 
+ * It has to return negative value if the first argument is less than the second, 
+ * positive value if the first argument is bigger than the second, 
+ * and zero if they are equal.
+ * @return A pointer to newly created array, or NULL in case of an error.
+ */
+struct sorted_array* sacreate(size_t elem_size, size_t max_elems, int (*compar)(void* a, void* b));
