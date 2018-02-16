70df767dec49a198dfe7e343c3a58e757d8d1e7b
BuildTools <unconfigured@null.spigotmc.org>
2018-02-16 20:46:28 +0300

    Added declaration and some description for struct sorted_array;

--- sorted_array.h
+++ sorted_array.h
@@ -20,4 +20,24 @@
  *   + sainext();
  *   + saiget();
  * - different variants of foreach() function.
+ * - saresort() function to fix broken order in case when it can change.
  */
+
+
+/** @struct sorted_array
+ * Structure representing a static sorted array.
+ *
+ * This array can contain elements of any size, but all elements in one array are of the same size.
+ * The elements are stored one by one in a buffer allocated in heap.
+ *
+ * The array is sorted, so there must be relations of order between elements. 
+ * When creating a new array, the caller of saalloc() must specify comparator function to define these relations.
+ * These relations must preserve across the whole lifetime of the array.
+ * 
+ * @note If you want to have an array of "heavy" elements, consider working with their pointers instead 
+ * (So, in fact, the array will be dynamic).
+ * @note If you encounter a situation when you need to apply some changes to the elements that can affect their order 
+ *  (e.g. You are storing pointers to some elements, and you also have pointers to them in other places of the program), 
+ *  call saresort() function to sort the array again.
+ */
+ struct sorted_array;
